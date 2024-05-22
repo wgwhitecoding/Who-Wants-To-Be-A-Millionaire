bb3defde175642d37947fbb9099b46215138d377
willcodinggod white <walidwhite@live.co.uk>
2024-05-22 16:36:23 +0000

    initial commit added basic html and created the work space files and folders

---
+++ assets/js/script.js
@@ -0,0 +1,62 @@
+const questions = [
+{
+    question: "What is the capital of France?",
+    answer: ("Paris", "London", "Berlin", "Madrid"),
+    correct: "Paris" 
+},
+{
+    question: "What is the largest planet in our solar system?",
+    answer: ("Mars", "Earth", "Jupiter", "Saturn"),
+    correct: "jupiter" 
+},
+{
+    question: "What year dd the Titanic sink?",
+    answer: ("1912", "1905", "1898", "1923"),
+    correct: "jupiter" 
+}, 
+{
+    question: "What is the capital of France",
+    answer: ("Paris", "London", "Berlin", "Madrid"),
+    correct: "jupiter" 
+},
+{
+    question: "What is the capital of France",
+    answer: ("Paris", "London", "Berlin", "Madrid"),
+    correct: "jupiter" 
+},
+{
+    question: "What is the capital of France",
+    answer: ("Paris", "London", "Berlin", "Madrid"),
+    correct: "jupiter" 
+},
+{
+    question: "What is the capital of France",
+    answer: ("Paris", "London", "Berlin", "Madrid"),
+    correct: "jupiter" 
+},
+{
+    question: "What is the capital of France",
+    answer: ("Paris", "London", "Berlin", "Madrid"),
+    correct: "jupiter" 
+},
+{
+    question: "What is the capital of France",
+    answer: ("Paris", "London", "Berlin", "Madrid"),
+    correct: "jupiter" 
+},
+{
+    question: "What is the capital of France",
+    answer: ("Paris", "London", "Berlin", "Madrid"),
+    correct: "jupiter" 
+},
+{
+    question: "What is the capital of France",
+    answer: ("Paris", "London", "Berlin", "Madrid"),
+    correct: "jupiter" 
+},
+]
+const prizeAmounts= [
+    100, 200, 300, 400, 500, 1000, 
+    2000, 4000, 8000, 16000,32000, 
+    64000, 125000, 2500000, 5000000, 10000000
+];
